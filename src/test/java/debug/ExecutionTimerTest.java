313512fa32a939133c620dd6710c1cec75b9c2a6
AdminOfThis <florianhild95@gmail.com>
2019-10-10 15:09:21 +0200

    Removed System out from Test Case

--- src/test/java/debug/ExecutionTimerTest.java
+++ src/test/java/debug/ExecutionTimerTest.java
@@ -2,10 +2,13 @@ package debug;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
-
 
 class ExecutionTimerTest {
 
@@ -13,7 +16,7 @@ class ExecutionTimerTest {
 	@Execution(ExecutionMode.CONCURRENT)
 	void testExecuteTime() {
 
-		int time = (int) ((Math.random() + 1.0) *  1000.0);
+		int time = (int) ((Math.random() + 1.0) * 1000.0);
 		Runnable e = () -> {
 			try {
 				Thread.sleep(time);
@@ -21,8 +24,17 @@ class ExecutionTimerTest {
 				e1.printStackTrace();
 			}
 		};
+		System.setOut(new PrintStream(new OutputStream() {
+
+			@Override
+			public void write(int arg0) throws IOException {
+
+			}
+		}));
 		long result = Math.round(ExecutionTimer.executeTime(e) / 1000000.0);
+		System.setOut(System.out);
 		long min = time - 10;
+
 		long max = time + 10;
 		assertTrue(min <= result && result <= max);
 	}
