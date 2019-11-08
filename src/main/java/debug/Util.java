84508efb2b774021347cab78d65e9e653cb7c497
AdminOfThis <florianhild95@gmail.com>
2019-11-08 14:25:28 +0100

    Removed Debug timing

--- src/main/java/debug/Util.java
+++ src/main/java/debug/Util.java
@@ -26,29 +26,4 @@ public final class Util {
 		}
 	}
 
-	public static long executeTime(Runnable e) {
-
-		return executeTime("The execution took", e);
-	}
-
-	public static long executeTime(String name, Runnable e) {
-		long timeBefore = System.nanoTime();
-		try {
-			e.run();
-		} catch (Exception ex) {
-			System.err.println("The task threw an exception: ");
-			ex.printStackTrace();
-		}
-		long timeAfter = System.nanoTime();
-		long duration = timeAfter - timeBefore;
-		StringBuilder sb = new StringBuilder(name + " ");
-		if (duration > 1000.0) {
-			sb.append(Math.round(duration / 1000000.0) + " ms");
-		} else {
-			sb.append(duration + " ns");
-		}
-		System.out.println(sb.toString());
-		return duration;
-	}
-
 }
