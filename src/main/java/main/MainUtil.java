5136b23ab73465e8e496ba3e044343d2567d09fa
AdminOfThis <florianhild95@gmail.com>
2020-01-27 08:59:21 +0100

    Added new Util class

---
+++ src/main/java/main/MainUtil.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class MainUtil {
+
+	private static final Logger LOG = LogManager.getLogger(MainUtil.class);
+
+	private MainUtil() {}
+
+	/**
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("resource")
+	public static boolean createRunningLockFile(String lockFile) {
+		boolean result = false;
+		try {
+			final File file = new File(lockFile);
+			final RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+			final FileLock fileLock = randomAccessFile.getChannel().tryLock();
+			if (fileLock != null) {
+				Runtime.getRuntime().addShutdownHook(new Thread() {
+					public void run() {
+						try {
+							fileLock.release();
+							randomAccessFile.close();
+							file.delete();
+						} catch (Exception e) {
+							LOG.error("Unable to remove lock file: " + fileLock, e);
+						}
+					}
+				});
+				return true;
+			}
+		} catch (Exception e) {
+			LOG.error("Unable to create and/or lock file", e);
+		}
+		return result;
+	}
+}
